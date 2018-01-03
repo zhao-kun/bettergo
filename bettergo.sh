1b3c746ea4dbcb12c48ae66f8b1c0cb6510c5e3a
Kun Zhao <zhaokun.km@gmail.com>
2018-01-03 15:20:48 +0800

    add CI support

--- bettergo.sh
+++ bettergo.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-USER=tmpuser
+set -e
 
-# Create a new group and user with the correct values
-groupadd -g $GID $USER
-useradd -mu $UID -g $GID $USER
-
-# Ensure /go is owned by the user
-chown -R $UID:$UID /go
-
-# Switch to the specified user's account and run the command
-sudo -EHu $USER env "PATH=$PATH" "$@"
+if [ $UID -eq 0 ]
+then
+    "$@"
+else
+    USER=tmpuser
+    
+    # Create a new group and user with the correct values
+    groupadd -g "$GID" "$USER"
+    useradd -mu $UID -g "$GID" $USER
+    
+    # Ensure /go is owned by the user
+    chown -R $UID:$UID /go
+    
+    # Switch to the specified user's account and run the command
+    sudo -EHu $USER env "PATH=$PATH" "$@"
+fi
